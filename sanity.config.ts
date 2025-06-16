80e6e05eaae5d2551f69efd5c2162b4169e192e4
Agustín <agusmartin.13.113@gmail.com>
2025-06-16 19:24:44 -0300

    Cambio a idioma español

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import {esESLocale} from '@sanity/locale-es-es'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +11,7 @@ export default defineConfig({
   projectId: 'wiylzlnn',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool(), visionTool(), esESLocale()],
 
   schema: {
     types: schemaTypes,
